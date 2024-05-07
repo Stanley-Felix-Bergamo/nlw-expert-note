80acd454d320e1419994aad5d1878f2221eec787
Stanley-Felix-Bergamo <stanley.felix.bergamo@outlook.com>
2024-05-07 14:37:08 -0300

    commit

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import logo from "./assets/logo-nlw-exper.svg";
 import { NewNoteCard } from "./components/new-note-card";
 import { NoteCard } from "./components/note-card";
@@ -10,7 +10,15 @@ interface Note {
 }
 
 export function App() {
-  const [notes, setNotes] = useState<Note[]>([]);
+  const [search, setSearch] = useState("");
+  const [notes, setNotes] = useState<Note[]>(() => {
+    const notesOnStorege = localStorage.getItem("notes");
+
+    if (notesOnStorege) {
+      return JSON.parse(notesOnStorege);
+    }
+    return [];
+  });
 
   const onNoteCreated = (content: string) => {
     const newNote = {
@@ -18,8 +26,24 @@ export function App() {
       date: new Date(),
       content,
     };
-    setNotes([newNote, ...notes]);
+    const notesArray = [newNote, ...notes];
+
+    setNotes(notesArray);
+
+    localStorage.setItem("notes", JSON.stringify(notesArray));
   };
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    const query = event.target.value;
+    setSearch(query);
+  };
+
+  const filteredNotes =
+    search != ""
+      ? notes.filter((note) =>
+          note.content.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )
+      : notes;
 
   return (
     <div className=" mx-auto max-w-6xl my-12 space-y-6">
@@ -30,12 +54,13 @@ export function App() {
           type="text"
           placeholder="Busque em suas notas..."
           className="w-full bg-transparent text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+          onChange={handleSearch}
         />
       </form>
       <div className="h-px bg-slate-700" />
       <div className="grid grid-cols-3 gap-6 auto-rows-[250px]">
         <NewNoteCard onNoteCreated={onNoteCreated} />
-        {notes.map((note) => {
+        {filteredNotes.map((note) => {
           return <NoteCard key={note.id} note={note} />;
         })}
       </div>
